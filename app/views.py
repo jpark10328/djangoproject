01e81a96088c4259d4fd899708823405cb4ee5a8
jeongseop park <jpark10328@gmail.com>
2022-10-21 10:09:55 +0900

    10/21 practice1

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    return render(request, "home.html")

66b5a039ed47d96cf75e9cc02fc6761c54f4c420
hyunsoo kim <pig4396@naver.com>
2022-10-27 14:15:43 +0900

    Django_10.27 review edit

--- user/models.py
+++ user/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser,PermissionsMixin
 
 # userid 를 장고가 사용하는 username 을 사용하지 않고 이메일로 변경
 class UserManager(BaseUserManager):
-    def _create_user(self, email, username, nickname, password, phone, rank=0, **extra_fields):
+    def _create_user(self, email, username, nickname, password, phone=None, rank=0, **extra_fields):
         """
         Create and save a user with the given username, email, and password.
         """
         if not email:
             raise ValueError("The given email must be set")
         email = self.normalize_email(email)
-        username = self.model.normalize_username(username)
+        # username = self.model.normalize_username(username)
         user = self.model(email=email, nickname=nickname, username=username, rank=rank, phone=phone, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -21,7 +21,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, username, nickname, password, phone, **extra_fields)
 
-    def create_superuser(self, email, password, phone, **extra_fields):
+    def create_superuser(self, email, password, phone=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -32,7 +32,7 @@ class UserManager(BaseUserManager):
 
         return self._create_user(email, "관리자", password, phone, **extra_fields)
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser,PermissionsMixin):
 
     RANK_CHOICES = [
         (0,'회원'),
@@ -44,6 +44,7 @@ class User(AbstractBaseUser):
     nickname = models.CharField(max_length=50, verbose_name="닉네임")
     rank = models.SmallIntegerField(choices = RANK_CHOICES, verbose_name="회원구분")
     phone = models.CharField(max_length=15, unique=True, null=True, verbose_name="전화번호")
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
@@ -52,3 +53,12 @@ class User(AbstractBaseUser):
 
     def __str__(self) -> str:
         return "<%d %s>" % (self.pk, self.email)
+
+
+    # def has_perm(self, perm, obj=None):
+    #     return True
+
+
+    # def has_module_perms(self, app_label):
+    #     return True
+    
